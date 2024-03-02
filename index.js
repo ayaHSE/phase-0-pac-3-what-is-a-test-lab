a29d2ae73b3329c21dd972714d07f14f831cee92
Aya Hassan <147654234+ayaHSE@users.noreply.github.com>
2024-03-02 16:55:01 +0300

    done

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-const name = "Joe";
-const height = 74;
-const message = `${name} is ${height} inches tall`;
-
-module.exports = {name, height, message};
+describe("Name", () => {
+    it('returns "Susan"', () => {
+      expect(name).toEqual("Susan"); // Update the expected value to "Susan"
+    });
+  });
+  
+  describe("Height", () => {
+    it("is less than 40 and greater than 0", () => {
+      expect(height).toBeLessThan(40); // Update the expected value to be less than 40
+      expect(height).toBeGreaterThan(0); // Add an assertion to check if height is greater than 0
+    });
+  });
+  
